73be2ecd216f714e740c603c556fef8a1aaaeed8
rizwanishaq <rizwanishaq@gmail.com>
2020-09-24 11:07:09 +0200

    Updates

---
+++ precache-manifest.81939253e9bf94867bb14e0124a92d7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc79f58b0bb327d034267c64fa43691",
+    "url": "/country-information/index.html"
+  },
+  {
+    "revision": "913b26ffd218e6496905",
+    "url": "/country-information/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2bfa38e7ef82bbfc4693",
+    "url": "/country-information/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "913b26ffd218e6496905",
+    "url": "/country-information/static/js/2.3169c2ba.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/country-information/static/js/2.3169c2ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bfa38e7ef82bbfc4693",
+    "url": "/country-information/static/js/main.8929e6d7.chunk.js"
+  },
+  {
+    "revision": "161ff69a894ff36fa80f",
+    "url": "/country-information/static/js/runtime-main.262c0546.js"
+  }
+]);
